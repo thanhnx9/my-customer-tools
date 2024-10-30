8a54ae59f7ee736ac302ad2db46cfa733065ce21
thanhnx9 <xuanthanh1228@gmail.com>
2024-10-30 22:58:23 +0700

    update crawler

--- crawlurls.sh
+++ crawlurls.sh
@@ -1,46 +1,60 @@
 #!/bin/bash
 
-# check input
-if [ -z "$1" ]; then
-  echo "Input domain.txt list file."
-  echo "Usage: $0 <domain_file>"
+# Check input
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Please provide a domain list file and target filename."
+  echo "Usage: $0 <domain_file> <target_file>"
   exit 1
 fi
 
-# var domain_file
+# Variables for domain_file and target_file
 domain_file="$1"
+target="$2"
 
-# Check file
+# Check if the domain_file exists
 if [ ! -f "$domain_file" ]; then
   echo "File $domain_file not found!"
   exit 1
 fi
 
-# create new folder
+# Create new folder
 folder="output_urls"
 if [ ! -d "$folder" ]; then
     mkdir "$folder"
-    echo "Create $folder"
+    echo "Folder $folder created"
 else
-    echo "$folder already exists!!"
-    echo "Start to crawl......"
+    echo "Folder $folder already exists!"
+    echo "Starting the crawling process..."
 fi
 
-# Running waybackurls
-echo "Waybackurls running..."
+# Run waybackurls
+echo "Running waybackurls..."
 cat "$domain_file" | waybackurls -no-subs > "$folder/url_waybackurls.txt"
 
-# Running httprobe
-echo "Httprobe running..."
-cat "$domain_file" | httprobe -p xlarge > "$folder/target.txt"
+# Add https:// and http:// prefixes to each domain and write to target
+echo "Adding https:// and http:// prefixes to each domain..."
+> "$folder/$target"  # Clear target file content if it exists
+while IFS= read -r domain; do
+  echo "https://$domain" >> "$folder/$target"
+  echo "http://$domain" >> "$folder/$target"
+done < "$domain_file"
 
-# Running hakrawler
-echo "Hakrawler running..."
-cat "$folder/target.txt" | hakrawler -subs > "$folder/url_hakrawler.txt"
+# Remove duplicates in the target file
+echo "Removing duplicate URLs in $target..."
+cat "$target" >> "$folder/$target
+cat "$folder/$target" | sort | uniq > "$folder/temp_target.txt"
+mv "$folder/temp_target.txt" "$folder/$target"
 
-# Running waymore
-echo "Waymore running..."
-waymore -i $folder/target.txt -oU $folder/url_waymore.txt
+# Run hakrawler
+echo "Running hakrawler..."
+cat "$folder/$target" | hakrawler -subs > "$folder/url_hakrawler.txt"
 
+# Run waymore
+echo "Running waymore..."
+waymore -i "$folder/$target" -oU "$folder/url_waymore.txt"
+
+# Consolidate URLs and remove duplicates
+echo "Consolidating URLs and removing duplicates..."
 cat "$folder/url_"* | uro > "$folder/all_urls.txt"
-echo "Finished!!"
+
+echo "Done!!"
